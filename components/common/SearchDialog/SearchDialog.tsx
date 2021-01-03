22175c613d846ec5bb795d0e89e3539ac3e2d2fd
Xuan Hien <hiennguyen9874@gmail.com>
2021-01-03 13:13:36 +0700

    Add debounce for search box

--- components/common/SearchDialog/SearchDialog.tsx
+++ components/common/SearchDialog/SearchDialog.tsx
@@ -1,13 +1,8 @@
-import React, {
-  useCallback,
-  useRef,
-  useState,
-  KeyboardEvent,
-  useEffect,
-} from 'react';
+import React, { useRef, useState, KeyboardEvent, useEffect } from 'react';
 import Link from 'next/link';
 import clsx from 'clsx';
 import axios from 'axios';
+import { debounce } from 'lodash';
 
 import type { getAllSearchPost } from '@utils/posts';
 import { useSearch, SET_ON_SEARCH } from '@Context/Search';
@@ -19,30 +14,40 @@ const SearchDialog: React.FunctionComponent = () => {
     [],
   );
   const [searchState, searchDispatch] = useSearch();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     inputRef.current.focus();
   });
 
-  const onChange = useCallback(
-    async (event: React.ChangeEvent<HTMLInputElement>) => {
-      setQuery(query);
-      if (query.length) {
-        const response = await axios.get(`/api/search?q=${event.target.value}`);
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        setResults(response.data.result);
-      } else {
-        setResults([]);
-      }
-    },
-    [query],
-  );
+  const getResultSearch = async (searchStr: string) => {
+    const response = await axios.get(`/api/search?q=${searchStr}`);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    setResults(response.data.result);
+    setLoading(false);
+  };
+
+  const debounceSearch = useRef(
+    debounce((nextValue) => getResultSearch(nextValue), 500),
+  ).current;
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    setLoading(true);
+    if (event.target.value.length) {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      debounceSearch(event.target.value);
+    } else {
+      setResults([]);
+    }
+  };
 
   const onKeyDown = (event: KeyboardEvent<HTMLDivElement>): void => {
     const key = event.keyCode ? event.keyCode : event.which;
     if (key === 27) {
       event.preventDefault();
       event.stopPropagation();
+      setResults([]);
       searchDispatch({
         type: SET_ON_SEARCH,
         payload: false,
@@ -69,6 +74,7 @@ const SearchDialog: React.FunctionComponent = () => {
         onClick={(event): void => {
           event.preventDefault();
           event.stopPropagation();
+          setResults([]);
           searchDispatch({
             type: SET_ON_SEARCH,
             payload: false,
@@ -84,6 +90,7 @@ const SearchDialog: React.FunctionComponent = () => {
             onClick={(event): void => {
               event.preventDefault();
               event.stopPropagation();
+              setResults([]);
               searchDispatch({
                 type: SET_ON_SEARCH,
                 payload: false,
@@ -104,22 +111,68 @@ const SearchDialog: React.FunctionComponent = () => {
         </div>
 
         <div className="flex flex-col items-center mt-4">
-          <input
-            type="search"
-            name="search"
-            placeholder="Search"
-            className="block w-full h-14 px-4 pr-10 text-lg focus:outline-none shadow-lg rounded-xl bg-gray-400 text-white placeholder-white dark:bg-gray-500"
-            autoComplete="off"
-            // eslint-disable-next-line jsx-a11y/no-autofocus
-            autoFocus
-            ref={inputRef}
-            value={query}
-            onChange={onChange}
-            onClick={(event): void => {
-              event.preventDefault();
-              event.stopPropagation();
-            }}
-          />
+          <div className="w-full flex flex-row justify-between items-center rounded-xl">
+            <input
+              type="search"
+              name="search"
+              placeholder="Search"
+              className="block w-full -mr-10 h-14 px-4 pr-10 text-lg focus:outline-none shadow-lg rounded-xl bg-gray-400 text-white placeholder-white "
+              autoComplete="off"
+              // eslint-disable-next-line jsx-a11y/no-autofocus
+              autoFocus
+              ref={inputRef}
+              value={query}
+              onChange={onChange}
+              onClick={(event): void => {
+                event.preventDefault();
+                event.stopPropagation();
+              }}
+            />
+            <span className="left-0 flex items-center pl-2 w-10 z-10">
+              <button type="submit">
+                {!loading && (
+                  <svg
+                    className="h-4 w-4 fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    version="1.1"
+                    id="Capa_1"
+                    x="0px"
+                    y="0px"
+                    viewBox="0 0 56.966 56.966"
+                    enableBackground="new 0 0 56.966 56.966"
+                    xmlSpace="preserve"
+                    width="512px"
+                    height="512px"
+                  >
+                    <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+                  </svg>
+                )}
+                {loading && (
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    />
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    />
+                  </svg>
+                )}
+              </button>
+            </span>
+          </div>
           <div className="grid grid-cols-1 w-full mt-2 rounded-xl px-4 py-2 whitespace-no-wrap overflow-visible divide-y divide-yellow-500 bg-gray-200 dark:bg-gray-600 dark:text-white">
             {results.length === 0 && <p>What are you looking for?</p>}
             {results.length > 0 &&
@@ -129,6 +182,7 @@ const SearchDialog: React.FunctionComponent = () => {
                     <button
                       type="button"
                       onClick={(): void => {
+                        setResults([]);
                         searchDispatch({
                           type: SET_ON_SEARCH,
                           payload: false,
